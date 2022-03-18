14964f9d1da8c6f7f3a7c50aa2ae0e77cc5e4838
fadhluibnu <f.ibnu.a1@gmail.com>
2022-03-19 00:32:41 +0700

    Dashboard

--- app/src/main/java/com/example/sement_shop_management/MainActivity.kt
+++ app/src/main/java/com/example/sement_shop_management/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.sement_shop_management
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         lightStatusBar(window)
+
+        button_create_data_penjualan.setOnClickListener {
+            startActivity(Intent(this, CreatePenjualanActivity::class.java))
+        }
+
+        button_create_data_pengeluaran.setOnClickListener {
+            startActivity(Intent(this, CreatePengeluaranActivity::class.java))
+        }
     }
 }
